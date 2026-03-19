f03e24144d66e41b0403c06b807678a6330a841e
Bruno <bruno2810g@gmail.com>
2026-03-18 22:42:57 -0300

    aula_1803

---
+++ script.js
@@ -0,0 +1,78 @@
+// // // console.group("Grupo de logs")
+// // //     console.log("teste")
+// // //     console.log("teste2")
+// // //     console.log("teste3")
+// // // console.groupEnd("Grupo de logs")
+
+// // // console.time("timer teste")
+
+// // // //colocar um processamento para ver o timer aumentando
+// // //     // for (let i = 0; i<5; i++){
+// // //     //     console.log("printando 5 vezes")
+// // //     // }
+
+// // // console.timeEnd("timer teste")
+
+// // // //variáveis
+
+// // // let texto = "olá"
+// // // console.log(`Tipo do texto ${texto} é: ${typeof(texto)}`)
+
+// // // let onoff = true
+// // // console.log(`Tipo da var ${onoff} é: ${typeof(onoff)}`)
+
+// // // let number = 10
+// // // console.log(`Tipo da var ${number} é: ${typeof(number)}`)
+
+// // // let nulo = null
+// // // console.log(`Tipo da var ${nulo} é: ${typeof(nulo)}`)
+
+// // // let bigInt = 99999999999999999999n
+// // // console.log(`Tipo da var ${bigInt} é: ${typeof(bigInt)}`)
+
+// // let task = {
+// //   id: 1,
+// //   title: "Aprender JavaScript",
+// //   completed: false,
+// //   assignee: {
+// //     name: "João",
+// //     email: "joao@gmail.com"
+// //   }
+// // };
+
+// // let tasks = [
+// //   {
+// //     id: 1,
+// //     title: "Aprender JavaScript",
+// //     completed: false,
+// //     assignee: {
+// //       name: "João",
+// //       email: "joao@gmail.com",
+// //     },
+// //   },
+// //   {
+// //     id: 1,
+// //     title: "Aprender JavaScript",
+// //     completed: false,
+// //     assignee: {
+// //       name: "João",
+// //       email: "joao@gmail.com",
+// //     },
+// //   },
+// //   {
+// //     id: 1,
+// //     title: "Aprender JavaScript",
+// //     completed: false,
+// //     assignee: {
+// //       name: "João",
+// //       email: "joao@gmail.com",
+// //     },
+// //   },
+// // ];
+
+// // console.table(tasks);
+// // console.log(typeof(tasks));
+
+// let data = new Date()
+// console.log(data);
+
